3e8d4d20598c108dd3ea597c4d2cb2e2ff1eab09
Dhruvxarch <Anuragvishwas19@gmail.com>
2023-01-11 22:18:17 +0530

    Find Unique element in an array

---
+++ Array/Find_Unique.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int FindUnique(int arr[], int size)
+{
+    int ans = 0;
+    for (int i = 0; i < size; i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    return ans;
+}
+
+int main()
+{
+    int array[] = {3, 1, 4, 1, 3};
+    int s = sizeof(array) / sizeof(array[0]);
+    cout << FindUnique(array, 5) << " ";
+    return 0;
+}
